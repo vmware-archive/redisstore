70dd6400bd4394ee627c5cbacb167060c5c1fba9
Tim Spann <tspann@gopivotal.com>
2014-06-24 20:45:39 -0400

    test 2

---
+++ src/main/java/com/gopivotal/redisstore/Constants.java
@@ -0,0 +1,12 @@
+package com.gopivotal.redisstore;
+
+public class Constants {
+
+	// refactor to CF 
+	// TODO:  standard
+    // todo:  make environment variable
+    // todo:  see best practices
+    public final static String queueName = "tspann-queue";
+    public final static String exchangeName = "tspann-exchange";
+
+}
